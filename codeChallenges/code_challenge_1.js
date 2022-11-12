50bebb46aabfff48b22084ecdc77769b40eaaf8d
Alex Alexander <a.alexander62442@gmail.com>
2022-11-11 23:04:03 -0700

    Add code challenge 1 solution

---
+++ codeChallenges/code_challenge_1.js
@@ -0,0 +1,27 @@
+// # Code Challenges
+
+// Below are 3 code challenges, increasing in complexity. You are required to complete 1 (of your choice) and encouraged to complete 2. If you are ahead on your schedule and have the time, you can absolutely complete all 3! For each challenge you want to work on, create a new JavaScript file inside this `codeChallenges` directory and choose a name that makes sense.
+
+// After completing your selected challenge(s), create a short video to share with your Slack small group as part of your deliverable for this lesson. In your video, please:
+// - Introduce the challenge
+// - Demonstrate how to live-code the solution
+// - Explain each line of code (this can be done while live-coding or after)
+// - Show that the program works as intended by running the code (you should run the code more than once to prove it works for all logic paths)
+// - Share a challenge or question you had when you solved it the first time
+
+// If you choose to complete more than one challenge, you only need to record one video for your small group.
+
+// ## Mild
+
+// Write a JavaScript program that defines a variable that stores a Number. The program should print out the String "even" if the Number is even, and the String "odd" if the Number is odd. Hint: You may need to do some extra research on the remainder operator (%) to solve this challenge.
+
+// var num = 23
+// if (num % )
+
+
+var num = 12
+if (num % 2 === 0) {
+    console.log("even")
+} else {
+    console.log("odd")
+}
